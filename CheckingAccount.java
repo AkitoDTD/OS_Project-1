1f6c0ab3829a6c73bda56d220467853efb353357
AfishSean <sean2602450849@gmail.com>
2026-02-27 19:16:19 +0800

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,26 +1,32 @@
 public class CheckingAccount extends BankAccount {
-
+	
+	// Free trades (a constant shared by all accounts)
     private static final int FREE_TRANSACTIONS = 3;
+    // Transaction fee for each trade beyond the limit
     private static final double TRANSACTION_FEE = 2.0;
-
+    // Record the current number of trades
     private int transactionCount = 0;
-
+    
+    // Initialize balance and number of transactions
     public CheckingAccount(double balance) {
         super(balance);
     }
-
+    
+    // Execute parent class logic and count the number of transactions
     @Override
     public void deposit(double amount) {
         super.deposit(amount);
         transactionCount++;
     }
 
+    // Execute withdrawal after verifying balance and count the number of times
     @Override
     public void withdraw(double amount) {
         super.withdraw(amount);
         transactionCount++;
     }
 
+    // Charges apply if the free limit is exceeded, and the counter will be reset
     public void deductFees() {
         if (transactionCount > FREE_TRANSACTIONS) {
             double fee = (transactionCount - FREE_TRANSACTIONS) * TRANSACTION_FEE;
