84d161198498810b525261e35449d0dc89b33664
BammLNG <143520967+BammLNG@users.noreply.github.com>
2026-02-28 16:34:45 +0700

    Refactor SavingsAccount for clarity and structure
    Refactor SavingsAccount class with improved comments and formatting.

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,19 +1,25 @@
 public class SavingsAccount extends BankAccount {
     private double interestRate;
 
+    // Construct function to initialize balance and interest rate
     public SavingsAccount(double balance, double interestRate) {
         super(balance);
         this.interestRate = interestRate;
     }
 
-    // Calculates interest growth. This is a background financial process.
+   // Calculate and add interest to the account function
     public void addInterest() {
+
+         // Check if balances are positive
         if (getBalance() > 0) {
             double interest = getBalance() * interestRate;
             updateBalance(interest);
             System.out.println("Interest added: $" + interest);
+
+        // Show message if balance is not enough
         } else {
             System.out.println("Insufficient Balance To Get Interest");
         }
     }
+
 }
